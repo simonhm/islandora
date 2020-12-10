183b7075b23f40afd30818ac56432f7a1ad86e3a
ezoller <zoller.eli@gmail.com>
2020-12-10 01:49:40 +0000

    fix url method

--- modules/islandora_iiif/src/Plugin/views/style/IIIFManifest.php
+++ modules/islandora_iiif/src/Plugin/views/style/IIIFManifest.php
@@ -186,7 +186,7 @@ class IIIFManifest extends StylePluginBase {
         foreach ($images as $image) {
           // Create the IIIF URL for this file
           // Visiting $iiif_url will resolve to the info.json for the image.
-          $file_url = $image->entity->toUrl()->toString();
+          $file_url = $image->entity->createFileUrl(FALSE);
           $mime_type = $image->entity->getMimeType();
           $iiif_url = rtrim($iiif_address, '/') . '/' . urlencode($file_url);
 
